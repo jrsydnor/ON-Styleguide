0bd4ba77633fcfbaef504635f9a6f2f7916e0526
Caitlyn Zahn <caitlyn.zahn@madebymunsters.com>
2018-02-05 14:52:54 -0500

    added modals and popouts

--- source/scripts/components/onPageScroll.js
+++ source/scripts/components/onPageScroll.js
@@ -18,10 +18,12 @@ $(document).ready(function () {
       $target = $(target);
 
       $('html, body').stop().animate({
-        'scrollTop': $target.offset().top+2
+        'scrollTop': $target.offset().top-150
       }, 500, 'swing', function () {
         window.location.hash = target;
         $(document).on("scroll", onScroll);
+
+        console.log("hello")
     });
   });
 });
